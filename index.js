f935770937ba615b6bcf45a9669991ea7df13491
hamedasemi <h-@live.se>
2020-06-14 19:16:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { createServer } from 'http';
+import SocketIO from 'socket.io';
+
+const server = createServer();
+
+const io = new SocketIO(server, {
+    serveClient: false,
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+io.use((socket, next) => {
+    let clientId = socket.handshake.headers['authorization'];
+    if (clientId === 'abcd') {
+        return next();
+    }
+    return next(new Error('authentication error'));
+});
+
+io.on('connection', (socket) => {
+    // console.log('connection')
+    socket.on('send-message', (payload) => {
+        setTimeout(() => {
+            io.emit('receive-message', payload)
+        }, 2000);
+    });
+})
