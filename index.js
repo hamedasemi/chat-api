06d3de021ed8aeaddcf48b3a48c44b50f13eeb5f
hamedasemi <h-@live.se>
2020-06-14 20:45:30 +0200

    Test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = new SocketIO(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": "http://192.168.0.36:8080",
+            "Access-Control-Allow-Origin": req.headers.origin,
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
